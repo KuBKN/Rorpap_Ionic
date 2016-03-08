aa666b35cf4699d8b12b2d35272acd59f425cfa2
Apiwat <nopapiwat@gmail.com>
2016-03-08 20:31:43 +0700

    Add ngRoute and route function

--- rorpap/www/js/app.js
+++ rorpap/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic', 'ngRoute'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,16 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function($routeProvider, $locationProvider ) {
+
+  $routeProvider.when('/',{
+        controller: 'HomeController',
+        templateUrl: './views/home.html'
+  }).when('/about', {
+        controller: 'AboutController',
+        templateUrl: './views/about.html'
+  });
+});
+
